1bd6735c76d12193a4bc0fa4fbaecf604d7dcd68
Freeman Jiang <freeman.jiang.ca@gmail.com>
2026-01-02 14:11:06 -0500

    meta

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -14,7 +14,25 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "bikemap.nyc",
-  description: "",
+  description:
+    "bikemap.nyc is a GPU-powered data visualization of 12 years of Citi Bike, New York City's biggest bike-sharing system.",
+  keywords: [
+    "Citi Bike",
+    "NYC bike visualization",
+    "bike share",
+    "data visualization",
+    "New York City",
+    "bikemap.nyc",
+  ],
+  authors: [{ name: "Freeman Jiang", url: "https://freemanjiang.com" }],
+  creator: "Freeman Jiang",
+  twitter: {
+    card: "summary_large_image",
+    title: "bikemap.nyc",
+    description:
+      "bikemap.nyc is a GPU-powered data visualization of 12 years of Citi Bike, New York City's biggest bike-sharing system.",
+    creator: "@freemanjiangg",
+  },
 };
 
 export default function RootLayout({
